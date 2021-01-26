7dcfb63cfce6c6379da74656bac2806559d72012
Jose Hurtado <hello@josehurtado.net>
2021-01-26 00:48:22 +0000

    Initial Function on Flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,25 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
+			getInitialData: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/amaf_jarkasi") //Contacts Hooks API
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						// Read the response as json
+						return response.json();
+					})
+					.then(function(responseAsJson) {
+						setStore({ contacts: responseAsJson });
+					})
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
+			}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
